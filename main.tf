75490ad39910803044bd64f22ddbda10ed91b8f8
Diana <80010947+Diasastr@users.noreply.github.com>
2024-03-21 00:46:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_security_group" "tf_sec_gr" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.jenkins_private_ip]
+    cidr_blocks = var.jenkins_private_ip
   }
 
   # Gitea HTTP/HTTPS access
